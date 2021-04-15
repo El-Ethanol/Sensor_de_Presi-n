1256cade825bbe8d1867fbced23f6df25515e079
El-Ethanol <ethancampos@ciencias.unam.mx>
2021-04-15 17:45:04 -0500

    Ventana Presión Lista

--- Parte_Lógica/VentanaPresión.py
+++ Parte_Lógica/VentanaPresión.py
@@ -1,30 +1,28 @@
-import serial as ser
-import time
 from PyQt5 import QtWidgets        
 from PyQt5.QtCore import QTime, QTimer
 from Parte_Gráfica.VentanaPresiónDiseño import Ui_PressureWindow
 from Parte_Lógica.VentanaPrincipal import MainWindow
 
-if MainWindow.pSerial.is_open:
-    MainWindow.pSerial.close()
-MainWindow.pSerial.open()
-MainWindow.pSerial.flushInput()
-MainWindow.pSerial.flushOutput()
-
-class WindowPresion(QtWidgets.QMainWindow, Ui_PressureWindow): #Pressure Window
+#Ventana Presión.
+class WindowPresion(QtWidgets.QMainWindow, Ui_PressureWindow):
     
-    def __init__(self, parent=None):
-        super(WindowPresion, self).__init__(parent)
+    def General(self, parent=None): 
+        super(WindowPresion, self).General(parent)
+        
+       #Importar parte gráfica
         self.setupUi(self)
+        
+       #Contador para actualizar presión
         timer=QTimer(self)
         timer.timeout.connect(self.showpressure)
         timer.start(0)
         
-    def showpressure(self):
+   #Actualizador de Presión    
+    def showpressure(self): 
         MainWindow.presion
         HoraActual=QTime.currentTime()
         Tiempo=HoraActual.toString('hh:mm:ss')
-        Press=str(presion)
+        Press=str(MainWindow.presion)
         self.labelp_2.setText("Hora:  " + Tiempo)
         self.labelp_3.setText("Presión:  " + Press + " Torr")
     
